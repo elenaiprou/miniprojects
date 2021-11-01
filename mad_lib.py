06256c83be2b5c0dcb6f95817da5c213defe6fe1
elenaiprou <elenaiprou@gmail.com>
2021-11-01 15:31:24 +0100

    mad lib

---
+++ mad_lib.py
@@ -0,0 +1,16 @@
+#Cuento mad_lib
+
+name =input("Escriba un nombre: ")
+work = input("Caul es tu porfesión: ")
+country = input("¿En que país quieres vivir?: ")
+
+print("""Hola: 
+Mi nombre es {} y soy un aventurero. Trabajo como {} para una revista de viajes. 
+Ayer mi jefe me mandó en avión a {}, para que escribiera sobre la experiencia de vivir en la selva durante una semana.
+Aquí, en la selva, hace mucho calor y llueve mucho, pero por lo menos no hace viento. 
+Hoy me he encontrado con las personas que viven en la selva. 
+Para poder dormir con ellos me han obligado a afeitarme el bigote y a cambiar el color rojo de mi pelo.
+Mañana me llevarán a cazar cocodrilos, en una barca de remo, pero antes me vestirán con una falda de paja y un collar de huesos. 
+Espero que también me dejen una lanza larga como la de ellos.
+Ahora me marcho a dormir a mi cabaña de madera.
+Prometo escribir pronto.""".format((name),(work),(country)))
